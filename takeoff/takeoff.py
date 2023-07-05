42b22fa138bf3cc74cc0fbf22186123a2fc8147a
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2023-07-05 10:01:17 +0800

    pxy

--- takeoff/takeoff.py
+++ takeoff/takeoff.py
@@ -1,6 +1,5 @@
 from dronekit import connect
 from arm_and_takeoff import arm_and_takeoff
-from to_release_area import to_release_area
 
 #connect to drone 
 connection_string ='/dev/ttyACM0' #Com of current FCM connection
@@ -8,5 +7,3 @@ print('Connectingto vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=False) 
 
 arm_and_takeoff(1, vehicle) #arm_and_takeoff(aTargetAltitude, vehicle)
-
-to_release_area(vehicle) #fly straight forward at 0.8m/s for 38sec
