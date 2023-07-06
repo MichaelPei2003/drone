bf9de589bd6fb3dcd52e7a11b590cac9d141acbc
yttt <2435467058@qq.com>
2023-07-06 19:25:29 +0800

    cyt

---
+++ test/simple_goto.py
@@ -0,0 +1,10 @@
+from dronekit import connect, VehicleMode
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from ..takeoff.arm_and_takeoff import arm_and_takeoff
+import time
+
+#connect to drone 
+connection_string ='/dev/ttyACM0' #Com of current FCM connection
+print('Connectingto vehicle on: %s' % connection_string) 
+vehicle = connect(connection_string, wait_ready=False) 
