d1c90a038c862b977bb7dc0bd1b9208ffe166cc2
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2023-07-06 08:40:10 +0800

    Modified simulation code

--- takeoff/sim_takeoff.py
+++ takeoff/sim_takeoff.py
@@ -1,5 +1,6 @@
-from dronekit import connect
+from dronekit import connect, VehicleMode
 from arm_and_takeoff import arm_and_takeoff
+import time
 
 #connect to drone
 connection_string ='192.168.43.169:14550' #RPI's IP, port is always 14550
@@ -7,3 +8,7 @@ print('Connectingto vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=False) 
 
 arm_and_takeoff(1, vehicle) #arm_and_takeoff(aTargetAltitude, vehicle)
+
+time.sleep(10)
+
+vehicle.mode = VehicleMode("RTL")
