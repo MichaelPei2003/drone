f08d233df9b90701e947445e396b4f6c18b9c0cb
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2023-07-08 21:50:34 +0800

    added new movement code

--- takeoff/send_body_ned_velocity.py
+++ takeoff/send_body_ned_velocity.py
@@ -5,7 +5,7 @@ def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, vehicle = None):
     if vehicle == None:
         print("Vehicle info unknown, please take over controls.")
             
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+    vehicle._master.mav.set_position_target_local_ned_send(#ned is currently abandoned
 
         0,       # time_boot_ms (not used)
 
@@ -20,7 +20,5 @@ def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, vehicle = None):
         velocity_x, velocity_y, velocity_z, # m/s
 
         0, 0, 0, # x, y, z acceleration
- 
-        0, 0)
 
-    vehicle.send_mavlink(msg)
+        0, 0)
