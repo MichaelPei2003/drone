f3c7e3ef60b4227772970f13433363e7736d9a07
MichaelPei2003 <71201708+MichaelPei2003@users.noreply.github.com>
2023-07-08 18:42:00 +0800

    updated MAV_CMD

--- README.md
+++ README.md
@@ -28,6 +28,7 @@
  ### 备忘 ###
  - 当前使用的飞控固件版本为arducopter 3.2.1
  - dronekit使用NED坐标系，z轴正方向指向地面，写参数时需要注意
+ - https://mavlink.io/en/messages/common.html (#73, #75, #76)
 
  ### 7.1更新 ###
  - **pxy**: 起飞、航行至投弹区代码未测试，识别坐标flag尚未定义 测试切换至master分支
--- takeoff/takeoff.py
+++ takeoff/takeoff.py
@@ -5,6 +5,7 @@ from arm_and_takeoff import arm_and_takeoff
 connection_string ='/dev/ttyACM0' #Com of current FCM connection
 print('Connectingto vehicle on: %s' % connection_string) 
 vehicle = connect(connection_string, wait_ready=False) 
+vehicle.message_factory
 
 arm_and_takeoff(1, vehicle) #arm_and_takeoff(aTargetAltitude, vehicle)
 
