f00a618ea4c31209b6061c362d3fa50133900bf3
yttt <2435467058@qq.com>
2023-07-09 21:58:29 +0800

    Update

--- scout/scout_example.py
+++ scout/scout_example.py
@@ -1,14 +1,19 @@
-import time 
 from dronekit import connect, VehicleMode, LocationGlobalRelative 
 
-from send_body_ned_velocity import send_body_ned_velocity
 from loiter_turns import loiter_turns
 
-from pymavlink import mavutil 
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from takeoff.arm_and_takeoff import arm_and_takeoff
  
+
 connection_string ='/dev/ttyACM0' 
 print('Connectingto vehicle on: %s' % connection_string) 
 vehicle = connect(connection_string, wait_ready=False) 
 
+arm_and_takeoff(2,vehicle)
+
 location=vehicle.location.global_relative_frame
 loiter_turns(vehicle,location)
+
+
