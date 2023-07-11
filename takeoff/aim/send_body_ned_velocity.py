3dda5740c95bf297d40efbdd5100d701cdab2917
dxy <1983220166@qq.com>
2023-07-11 19:53:38 +0800

    藻门

--- takeoff/aim/send_body_ned_velocity.py
+++ takeoff/aim/send_body_ned_velocity.py
@@ -5,30 +5,39 @@ def send_body_ned_velocity(vehicle = None):
     if vehicle == None:
         print("Vehicle info unknown, please take over controls.")
     
-    #weird behaviers            
-    # vehicle._master.mav.set_position_target_local_ned_send(#ned is currently abandoned
+#    weird behaviers      
+    velocity_x = 1
+    velocity_y = 0
+    velocity_z = 0      
+    vehicle._master.mav.command_long_send(
+        0, 0,
+        mavutil.mavlink.MAV_CMD_DO_SET_REVERSE,
+        0,
+        1
+    )
+    vehicle._master.mav.set_position_target_local_ned_send(#ned is currently abandoned
 
-    #     0,       # time_boot_ms (not used)
+        0,       # time_boot_ms (not used)
 
-    #     0, 0,    # target system, target component
+        0, 0,    # target system, target component
 
-    #     mavutil.mavlink.MAV_FRAME_BODY_NED,
+        mavutil.mavlink.MAV_FRAME_BODY_NED,
 
-    #     0b0000111111000111, # type_mask
+        0b0000111111000111, # type_mask
 
-    #     0, 0, 0, # x, y, z positions (not used)
+        0, 0, 0, # x, y, z positions (not used)
 
-    #     velocity_x, velocity_y, velocity_z, # m/s
+        velocity_x, velocity_y, velocity_z, # m/s
 
-    #     0, 0, 0, # x, y, z acceleration
+        0, 0, 0, # x, y, z acceleration
 
-    #     0, 0)
+        0, 0)
     
     
     #doesn't move
     # print("banking right")
     # vehicle._master.mav.command_long_send(
-    #     1, 1,
+    #     0, 0,
     #     mavutil.mavlink.MAV_CMD_NAV_ATTITUDE_TIME,
     #     0,  # confirmation
     #     10,  # roll angle (degrees)
@@ -37,9 +46,9 @@ def send_body_ned_velocity(vehicle = None):
     #     0.5, # thrust (0-1)
     #     0, 0, 0)
 
-        vehicle._master.mav.set_attitude_target_send(
-            0,
-            0, 0,
-            0b11000111, # type_mask
+        # vehicle._master.mav.set_attitude_target_send(
+        #     0,
+        #     0, 0,
+        #     0b11000111, # type_mask
             
-        )
+        # )
