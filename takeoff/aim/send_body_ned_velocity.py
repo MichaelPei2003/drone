78706851dd2a4fc5562f189bb8ec8d9fbd7cdd92
dxy <1983220166@qq.com>
2023-07-11 21:21:09 +0800

    藻门！

--- takeoff/aim/send_body_ned_velocity.py
+++ takeoff/aim/send_body_ned_velocity.py
@@ -2,27 +2,27 @@ from pymavlink import mavutil
 
 def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, vehicle = None):
     
-    if vehicle == None:
-        print("Vehicle info unknown, please take over controls.")
+#     if vehicle == None:
+#         print("Vehicle info unknown, please take over controls.")
     
-#    weird behaviers      
-    vehicle._master.mav.set_position_target_local_ned_send(
+# #    weird behaviers      
+#     vehicle._master.mav.set_position_target_local_ned_send(
 
-        0,       # time_boot_ms (not used)
+#         0,       # time_boot_ms (not used)
 
-        0, 0,    # target system, target component
+#         0, 0,    # target system, target component
 
-        mavutil.mavlink.MAV_FRAME_BODY_NED,
+#         mavutil.mavlink.MAV_FRAME_BODY_NED,
 
-        0b0000111111000111, # type_mask
+#         0b0000111111000111, # type_mask
 
-        0, 0, 0, # x, y, z positions (not used)
+#         0, 0, 0, # x, y, z positions (not used)
 
-        velocity_x, velocity_y, velocity_z, # m/s
+#         velocity_x, velocity_y, velocity_z, # m/s
 
-        0, 0, 0, # x, y, z acceleration
+#         0, 0, 0, # x, y, z acceleration
 
-        0, 0)
+#         0, 0)
     
     
     #doesn't move
@@ -37,9 +37,9 @@ def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, vehicle = None):
     #     0.6, # thrust (0-1)
     #     0, 0, 0)
 
-        # vehicle._master.mav.set_attitude_target_send(
-        #     0,
-        #     0, 0,
-        #     0b11000111, # type_mask
-            
-        # )
+    vehicle._master.mav.set_attitude_target_send(
+        0,
+        0, 0,
+        0b11000111, # type_mask
+        
+    )
