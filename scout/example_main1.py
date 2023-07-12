a52ea978c8fc4c87253576fcb82aff5dc77ac080
yttt <2435467058@qq.com>
2023-07-12 09:03:41 +0800

    update example_main1

--- scout/example_main1.py
+++ scout/example_main1.py
@@ -21,96 +21,115 @@ arm_and_takeoff(5,vehicle)
 #路线2
 #←↑→
 
-#向左旋转90度
-send_body_angle(-90,5,vehicle)
-time.sleep(3)
-print("turn left 90 degree overed")
-
-#向前运动5m
-start_time = time.time()
-duration = 5    #运动的持续时间
-v_x = 1    #飞机在前后方向上的速度，前为正
-v_y = 0    #飞机在左右方向上的速度，右为正
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
-# if cyl == 1 :    #检测到圆筒
-#     scout(v_x,v_y,duration,start_time,vehicle)
+send_body_ned_velocity(0,-1,0,7,vehicle)
 time.sleep(5)
-print("5m overed")
 
-#向右旋转90度
-send_body_angle(90,5,vehicle)
-time.sleep(3)
-print("turn right 90 degree overed")
-
-#向前运动2m
-start_time = time.time()
-duration = 2
-v_x = 1
-v_y = 0
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+send_body_ned_velocity(1,0,0,2,vehicle)
 time.sleep(5)
-print("2m overed")
 
-#向右旋转90度
-send_body_angle(90,5,vehicle)
-time.sleep(3)
-print("turn right 90 degree overed")
-
-#向前运动5m
-start_time = time.time()
-duration = 5
-v_x = 1
-v_y = 0
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
-# if cyl == 1 :
-#     scout(v_x,v_y,duration,start_time,vehicle)
+send_body_ned_velocity(1,0,0,7,vehicle)
 time.sleep(5)
-print("5m overed")
 
-#调个头
-send_body_angle(180,5,vehicle)
-time.sleep(3)
-print("turn backward")
-
-#向前运动5m
-start_time = time.time()
-duration = 5    #运动的持续时间
-v_x = 1    #飞机在前后方向上的速度，前为正
-v_y = 0    #飞机在左右方向上的速度，右为正
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
-# if cyl == 1 :    #检测到圆筒
-#     scout(v_x,v_y,duration,start_time,vehicle)
+send_body_ned_velocity(0,-1,0,7,vehicle)
 time.sleep(5)
-print("5m overed")
 
-#向左旋转90度
-send_body_angle(-90,5,vehicle)
-time.sleep(3)
-print("turn left 90 degree overed")
-
-#向前运动2m
-start_time = time.time()
-duration = 2
-v_x = 1
-v_y = 0
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+send_body_ned_velocity(-1,0,0,2,vehicle)
 time.sleep(5)
-print("2m overed")
 
-#向左旋转90度
-send_body_angle(-90,5,vehicle)
-time.sleep(3)
-print("turn left 90 degree overed")
-
-#向前运动5m
-start_time = time.time()
-duration = 5
-v_x = 1
-v_y = 0
-send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
-# if cyl == 1 :
-#     scout(v_x,v_y,duration,start_time,vehicle)
+send_body_ned_velocity(1,0,0,7,vehicle)
 time.sleep(5)
-print("5m overed")
+
+
+# #向左旋转90度
+# send_body_angle(-90,5,vehicle)
+# time.sleep(3)
+# print("turn left 90 degree overed")
+
+# #向前运动5m
+# start_time = time.time()
+# duration = 5    #运动的持续时间
+# v_x = 1    #飞机在前后方向上的速度，前为正
+# v_y = 0    #飞机在左右方向上的速度，右为正
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# # if cyl == 1 :    #检测到圆筒
+# #     scout(v_x,v_y,duration,start_time,vehicle)
+# time.sleep(5)
+# print("5m overed")
+
+# #向右旋转90度
+# send_body_angle(90,5,vehicle)
+# time.sleep(3)
+# print("turn right 90 degree overed")
+
+# #向前运动2m
+# start_time = time.time()
+# duration = 2
+# v_x = 1
+# v_y = 0
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# time.sleep(5)
+# print("2m overed")
+
+# #向右旋转90度
+# send_body_angle(90,5,vehicle)
+# time.sleep(3)
+# print("turn right 90 degree overed")
+
+# #向前运动5m
+# start_time = time.time()
+# duration = 5
+# v_x = 1
+# v_y = 0
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# # if cyl == 1 :
+# #     scout(v_x,v_y,duration,start_time,vehicle)
+# time.sleep(5)
+# print("5m overed")
+
+# #调个头
+# send_body_angle(180,5,vehicle)
+# time.sleep(3)
+# print("turn backward")
+
+# #向前运动5m
+# start_time = time.time()
+# duration = 5    #运动的持续时间
+# v_x = 1    #飞机在前后方向上的速度，前为正
+# v_y = 0    #飞机在左右方向上的速度，右为正
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# # if cyl == 1 :    #检测到圆筒
+# #     scout(v_x,v_y,duration,start_time,vehicle)
+# time.sleep(5)
+# print("5m overed")
+
+# #向左旋转90度
+# send_body_angle(-90,5,vehicle)
+# time.sleep(3)
+# print("turn left 90 degree overed")
+
+# #向前运动2m
+# start_time = time.time()
+# duration = 2
+# v_x = 1
+# v_y = 0
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# time.sleep(5)
+# print("2m overed")
+
+# #向左旋转90度
+# send_body_angle(-90,5,vehicle)
+# time.sleep(3)
+# print("turn left 90 degree overed")
+
+# #向前运动5m
+# start_time = time.time()
+# duration = 5
+# v_x = 1
+# v_y = 0
+# send_body_ned_velocity(v_x,v_y,0,duration,vehicle)
+# # if cyl == 1 :
+# #     scout(v_x,v_y,duration,start_time,vehicle)
+# time.sleep(5)
+# print("5m overed")
 
 vehicle.mode = VehicleMode("RTL")
