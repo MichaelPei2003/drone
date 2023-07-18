330870d66f22930bf619fd4ce87079113f5a3271
HJIWE <121221059+HJIWE@users.noreply.github.com>
2023-07-18 11:40:12 +0800

    Add files via upload

--- RTL/shot_main.py
+++ RTL/shot_main.py
@@ -9,10 +9,11 @@ connection_string ='/dev/tty/ACM0' #Com of current FCM connection
 print('Connectingto vehicle on: %s' % connection_string) 
 vehicle = connect (connection_string, wait_ready=True) 
 
-arm_and_takeoff(3,vehicle)
+arm_and_takeoff(2,vehicle)
 send_body_ned_velocity(0.4,0,0,10,vehicle)
 time.sleep(5)
 vehicle.mode = VehicleMode("RTL")
+time.sleep(2)
 shot(vehicle)
 
 print("已完成投弹，执行下一步侦查指令...")
