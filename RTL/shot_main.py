ccdb17b89ebf1187e70aa67ef6cdcf7fef76010b
HJIWE <121221059+HJIWE@users.noreply.github.com>
2023-07-19 08:09:08 +0800

    Update shot_main.py

--- RTL/shot_main.py
+++ RTL/shot_main.py
@@ -9,11 +9,7 @@ connection_string ='/dev/ttyACM0' #Com of current FCM connection
 print('Connectingto vehicle on: %s' % connection_string) 
 vehicle = connect (connection_string, wait_ready=True) 
 
-arm_and_takeoff(2,vehicle)
-send_body_ned_velocity(0.4,0,0,10,vehicle)
-time.sleep(5)
-vehicle.mode = VehicleMode("RTL")
-time.sleep(2)
+
 shot(vehicle)
 
 print("已完成投弹，执行下一步侦查指令...")
